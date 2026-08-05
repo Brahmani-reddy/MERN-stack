f9eebe85af0bed1d58325336149c97d556729a3f
Brahmani <brahmanisunkara@gmail.com>
2026-08-05 20:27:04 +0530

    Initial commit

---
+++ b5intro.ts
@@ -0,0 +1,12 @@
+let firstName: string = "Brahmani";
+let lastName: string = "Sunkara";
+let fatherName: string = "Krishna reddy";
+
+let myAge : number = 18;
+let fatherAge : number = 50;
+
+let isMajor : boolean = true;
+
+console.log("Hi, this is"+firstName+" "+lastName+".");
+console.log("My father name is "+fatherName+" and he is "+fatherAge+" now.");
+console.log("I am "+myAge+" years old and I am a major.");
