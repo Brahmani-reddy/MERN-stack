4ca12a14db352edee7088a216e27ff852709b8ac
Brahmani_Reddy_ <brahmanisunkara@gmail.com>
2026-08-11 12:46:52 +0530

    Readonly and static properties

---
+++ Week_3/3_readonly_static.ts
@@ -0,0 +1,18 @@
+// Readonly & static properties.
+class Student {
+  readonly name: string;
+  age: number;
+  static college: string = "SVECW";
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+  introduce(): void {
+    console.log("My name is " + this.name);
+    console.log("I am " + this.age + " years old");
+    console.log("College: " + Student.college);
+  }
+}
+
+let student = new Student("Brahmani", 18);
+student.introduce();
