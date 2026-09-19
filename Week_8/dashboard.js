1e1615888583db430cb3e8d11fffab264e59eb15
Brahmani_Reddy_ <brahmanisunkara@gmail.com>
2026-09-19 08:58:49 +0530

    Dashboard HTML template

---
+++ Week_8/dashboard.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dashboard</title>
+</head>
+<body>
+
+    <h1>Welcome to Dashboard</h1>
+
+    <p>Hello, <%= username %>!</p>
+
+    <p>You are successfully logged in.</p>
+
+    <a href="/logout">
+        <button>Logout</button>
+    </a>
+
+</body>
+</html>
